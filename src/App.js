de66aab320b44b86ae72a6770b3c1ffbad72919a
valliappan Lakshmanan <valliappanlakshmanan@valliappans-MacBook-Air.local>
2021-01-09 16:58:13 -0500

    created main component for table and turned app component into a class component so that I could store state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Header from "./component/Header";
 import Search from "./component/Search";
+import Table from "./component/Table";
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Search/>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    employeeList:{}
+  }
+  
+  render(){
+    return(
+      <div className="App">
+        <Header/>
+        <Search/>
+        <Table/>
+      </div>
+    );
+  } 
 }
 
 export default App;
---
+++ src/component/Table.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Table() {
+    return (
+        <table className="table table-dark justify-content-center">
+            <thead>
+                <tr>
+                    <th scope="col"></th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Username</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    )
+}
+
+export default Table;
