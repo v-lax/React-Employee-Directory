4808dcfa1c33e2e44b94ad0a7ca27a0cfaf61b54
valliappan Lakshmanan <valliappanlakshmanan@valliappans-MacBook-Air.local>
2021-01-09 23:34:43 -0500

    added in loading component and used axios to pull in data from random user api and then was able to store that in the state

--- src/App.js
+++ src/App.js
@@ -2,21 +2,46 @@ import React from "react";
 import Header from "./component/Header";
 import Search from "./component/Search";
 import Table from "./component/Table";
+import axios from "axios";
+import "./style.css"
 
 class App extends React.Component {
   state = {
-    employeeList:{}
+    loading: true,
+    employeeList: {}
   }
-  
-  render(){
-    return(
+
+  async componentDidMount(){
+    let response = await axios({
+      method:'get',
+      url:'https://randomuser.me/api/?results=50',
+      responseType:'stream'
+    })
+
+    this.setState({
+      loading: false,
+      employeeList:response.data.results
+    })
+  }
+
+  render() {
+    if (this.state.loading) {
+      return (
+        <div className="spinner">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      )
+    }
+    return (
       <div className="App">
-        <Header/>
-        <Search/>
-        <Table/>
+        <Header />
+        <Search />
+        <Table />
       </div>
     );
-  } 
+  }
 }
 
 export default App;
